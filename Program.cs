20d31a122b7a5bf657eae1ec3644df00072bac5e
codemsbr <mi7pum53j@code.edu.az>
2023-10-18 14:52:51 -0700

    create projects

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 12, 21, 23, 24, -1, -2, -3, 4, 5, };
+            ConvertNegativesToPositives(ref myArray);
+
+        }
+
+        static void ConvertNegativesToPositives(ref int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] < 0)
+                    myArray[i] *= -1;
+            }
+        }
+    }
+}
